3f88a8a9574bcb746f9bc69f909c4c7823ba41e3
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-04-10 21:02:55 +0530

    event details

---
+++ screens/EventsDetails.js
@@ -0,0 +1,102 @@
+import { Link } from "@react-navigation/native";
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import BackIcon from "../assets/back.png";
+import PlaceholderImg from "../assets/placeholder.jpg";
+import { useNavigation } from "@react-navigation/native";
+
+import FocusedStatusBar from "../components/FocusedStatusBar";
+
+function EventsDetails({ route }) {
+  const data = route.params;
+  return (
+    <>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <SafeAreaView>
+          <Image source={{ uri: data.image }} style={styles.image} />
+          <View style={styles.inner_container}>
+            <View style={styles.header}>
+              <Link to={{ screen: "HomePage" }} style={styles.backBtn}>
+                <Image source={BackIcon} />
+                <Text style={{ fontSize: 14 }}>Back</Text>
+              </Link>
+              <Text style={{ fontSize: 24 }}>{data.title}</Text>
+            </View>
+            <View>
+              <Text>{data.location}</Text>
+            </View>
+            <View style={styles.whatdotheydo}>
+              <Text>There is an event {data.location}. Please join us!</Text>
+            </View>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={{ textAlign: "center", color: "white" }}>
+                Contact
+              </Text>
+            </TouchableOpacity>
+            <View>
+              <Text style={{ textAlign: "center", paddingVertical: 10 }}>
+                OR
+              </Text>
+            </View>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={{ textAlign: "center", color: "white" }}>
+                Message
+              </Text>
+            </TouchableOpacity>
+            {/* map */}
+          </View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  backBtn: {
+    marginTop: 12,
+    marginRight: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  btn: {
+    borderRadius: 12,
+    backgroundColor: "#F95F5F",
+    paddingVertical: 10,
+    paddingHorizontal: 70,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    height: 52,
+  },
+  header: {
+    flexDirection: "row",
+    paddingBottom: 25,
+  },
+  inner_container: {
+    padding: 20,
+  },
+  whatdotheydo: {
+    padding: 10,
+  },
+});
+
+export default EventsDetails;
